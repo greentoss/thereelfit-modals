fccf1a4af2c4c7651a45929bcde5fb6ea8f186fb
greentoss <mrsoberman1@gmail.com>
2024-07-10 16:01:57 +0300

    open andclose correctly fixed

--- overlay/init.js
+++ overlay/init.js
@@ -31,6 +31,8 @@ document.addEventListener("DOMContentLoaded", function () {
       const iframe = modalContent.querySelector(".modal-iframe");
       const closeBtn = modalContent.querySelector("#close");
   
+      let isModalOpen = false;
+  
       // Function to open modal
       function openModal() {
         pauseVideo();
@@ -38,6 +40,7 @@ document.addEventListener("DOMContentLoaded", function () {
         modal.style.display = "block";
         modalContent.classList.add('show-modal');
         modalContent.classList.remove('hide-modal');
+        isModalOpen = true;
       }
   
       // Function to close modal
@@ -48,22 +51,26 @@ document.addEventListener("DOMContentLoaded", function () {
           modal.style.display = "none";
           playVideo();
         }, 1000); // Match the duration of unfoldOut animation
+        isModalOpen = false;
+      }
+  
+      // Function to toggle modal
+      function toggleModal() {
+        if (isModalOpen) {
+          closeModal();
+        } else {
+          openModal();
+        }
       }
   
       // Event listeners
       overlay.addEventListener("click", function (event) {
         if (event.target === overlay) {
-          openModal();
+          toggleModal();
         }
       });
   
       closeBtn.addEventListener("click", closeModal);
-  
-      window.addEventListener("click", function (event) {
-        if (event.target === modal) {
-          closeModal();
-        }
-      });
   
       video.addEventListener("timeupdate", function () {
         if (config.timestamps.includes(Math.floor(video.currentTime))) {
